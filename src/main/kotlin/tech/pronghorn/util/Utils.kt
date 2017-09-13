aad72c3e768c509cb4932049e38cfaac8efb6920
Chris Turner <chris@pronghorn.tech>
2017-09-12 19:45:41 -0600

    Moving a common util back to this repository.

--- src/main/kotlin/tech/pronghorn/util/Utils.kt
+++ src/main/kotlin/tech/pronghorn/util/Utils.kt
@@ -8,3 +8,14 @@ fun Exception.stackTraceToString(): String {
     printStackTrace(PrintWriter(exceptionWriter))
     return exceptionWriter.toString()
 }
+
+fun runAllIgnoringExceptions(vararg blocks: () -> Unit) {
+    blocks.forEach { block ->
+        try {
+            block()
+        }
+        catch (ex: Exception) {
+            // no-op
+        }
+    }
+}
